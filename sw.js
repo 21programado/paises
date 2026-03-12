d54572324bfbe35dc1c8c99be36582628d122a46
21programado <21programado@gmail.com>
2026-03-12 14:37:43 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="paises-v1";
+const CACHE="paises-v2";
 
 const FILES=[
 "./",
@@ -7,23 +7,13 @@ const FILES=[
 ];
 
 self.addEventListener("install",e=>{
-
 e.waitUntil(
 caches.open(CACHE).then(cache=>cache.addAll(FILES))
 );
-
 });
 
 self.addEventListener("fetch",e=>{
-
 e.respondWith(
-
-caches.match(e.request).then(r=>{
-
-return r || fetch(e.request);
-
-})
-
+caches.match(e.request).then(r=>r || fetch(e.request))
 );
-
 });
